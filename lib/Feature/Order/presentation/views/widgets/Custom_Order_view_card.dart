043109f5d2d4980b4fca82e74d0f363baf30e9e5
Omar Khaled <omarkhaled.6403@gmail.com>
2026-04-01 14:02:44 +0200

    create the order view and end of it

--- lib/Feature/Order/presentation/views/widgets/Custom_Order_view_card.dart
+++ lib/Feature/Order/presentation/views/widgets/Custom_Order_view_card.dart
@@ -1,38 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:hungry_food/Core/utils/Assets.dart';
-import 'package:hungry_food/Feature/Cart/presentation/views/Cart%20View/widgets/Custom_increment_Decrement_Btn.dart';
+import 'package:hungry_food/Feature/shared/CustomButton.dart';
 import 'package:hungry_food/Feature/shared/Custom_text.dart';
 
-class CustomCartViewCartItem extends StatelessWidget {
-  const CustomCartViewCartItem({super.key});
+class CustomOrderViewCard extends StatelessWidget {
+  const CustomOrderViewCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Card(
       color: Colors.white,
       elevation: 5,
-      child: Row(
+      child: Column(
         children: [
           Gap(8),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 8.0),
-            child: Column(
+            padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8),
+            child: Row(
               // crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Image.asset(AssetsDate.product, width: 110),
-                Gap(5),
+                Gap(20),
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     CustomText(
-                      text: "Hamburger",
+                      text: "Hamburger Hamburger",
                       size: 16,
                       color: Colors.black,
                       weight: FontWeight.w600,
                     ),
                     CustomText(
-                      text: "Veggie Burger",
+                      text: "QTY: 3x",
+                      size: 14,
+                      color: Colors.black,
+                      weight: FontWeight.w400,
+                    ),
+                    CustomText(
+                      text: "Price: 20\$",
                       size: 14,
                       color: Colors.black,
                       weight: FontWeight.w400,
@@ -42,10 +48,16 @@ class CustomCartViewCartItem extends StatelessWidget {
               ],
             ),
           ),
-
-          Spacer(),
-          CustomIncrementandDecremetnBtn(),
-          Gap(16),
+          Gap(10),
+          CustomButton(
+            width: 400,
+            heaight: 90,
+            text: "Order Again",
+            textcolor: Colors.white,
+            backcolor: Colors.grey.shade400,
+            onPressed: () {},
+          ),
+          Gap(10),
         ],
       ),
     );
--- lib/Feature/Order/presentation/views/widgets/Order_view_body.dart
+++ lib/Feature/Order/presentation/views/widgets/Order_view_body.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:hungry_food/Core/utils/Assets.dart';
+import 'package:hungry_food/Feature/Cart/presentation/views/Cart%20View/widgets/Custom_Cart_view_Card_item.dart';
+import 'package:hungry_food/Feature/Order/presentation/views/widgets/Custom_Order_view_card.dart';
+import 'package:hungry_food/Feature/shared/CustomButton.dart';
+import 'package:hungry_food/Feature/shared/Custom_text.dart';
 
 class OrderViewbody extends StatelessWidget {
   const OrderViewbody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text("order")));
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 24),
+        child: ListView.builder(
+          itemCount: 20,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: CustomOrderViewCard(),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
